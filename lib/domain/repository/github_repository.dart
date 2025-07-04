ba2aaa08f94bd69ba140af19130f3f42840dbde0
Rerurate514 <rerurateyuto@gmail.com>
2025-07-04 23:41:21 +0900

    Repositoryインターフェースにquery変数を追加

--- lib/domain/repository/github_repository.dart
+++ lib/domain/repository/github_repository.dart
@@ -1,7 +1,7 @@
 import 'package:clean_arch_gb/domain/entity/repository.dart';
 
 abstract class GithubRepository {
-  Future<List<Repository>> searchRepositories();
+  Future<List<Repository>> searchRepositories(String query);
   Future<Repository> getRepositoryDetails(String owner, String repositoryName);
   void dispose();
 }
